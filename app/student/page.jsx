34e14732e00a77836b500d94cdf249df8fd44494
Ertuğ Akman <ertug3470@gmail.com>
2024-11-19 20:41:04 +0000

    The structure of student portal content has been created.

--- app/student/page.jsx
+++ app/student/page.jsx
@@ -1,3 +1,5 @@
+import PageCard from "@/components/StudentPage/PageCard";
+
 // app/page.js
 export default function Student() {
     const getGreeting = () => {
@@ -14,13 +16,30 @@ export default function Student() {
 
     return (
         <div>
-            <div className="flex items-center justify-between w-full p-5 border-b-2">
-                <h1 className="text-3xl ml-2 font-bold text-[#912A3E]">
+            <div className="flex sm:items-center sm:flex-row flex-col sm:gap-0 justify-between w-full p-5 border-b-2">
+                <h1 className="text-3xl ml-0 sm:ml-2 font-bold text-[#912A3E]">
                     Student Portal
                 </h1>
                 <p className="mr-4 text-lg text-gray-600">
                     {`${getGreeting()} Ertug`} 👋
                 </p>
+            </div>
+            <div className="w-full h-full flex justify-between">
+                <div className="w-[50%] flex flex-wrap h-50 gap-4 pl-6 bg-blue-300">
+                    <div>
+                        <PageCard />
+                    </div>
+                    <div>
+                        <PageCard />
+                    </div>
+                    <div>
+                        <PageCard />
+                    </div>
+                    <div>
+                        <PageCard />
+                    </div>
+                </div>
+                <div className="w-[50%] h-50 bg-green-300">Timetable</div>
             </div>
         </div>
     );
---
+++ components/StudentPage/PageCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageCard = () => {
+    return <div className="h-[120px] w-[260px] bg-blue-500">PageCard</div>;
+};
+
+export default PageCard;
